bef3d0b0e7c9209444a600a0d145ee27ab5df316
Connor White <connorwhitepdx@gmail.com>
2022-07-02 17:17:42 -0700

    refactor: Nav categories filter photos from PhotoList

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,20 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-
 import About from '..';
 
 afterEach(cleanup);
 
-describe('About component', () => {
-    // First Test
-    test('renders',() => {
-        render(<About />);
-    });
-    //Second Test
-    test('matches snapshot Dom node structure', () => {
-        //render About
-        const {asFragment} = render(<About />);
-        expect(asFragment()).toMatchSnapshot();
-    });
+describe('About component renders', () => {
+  it('renders', () => {
+    render(<About />);
+  });
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,36 +1,37 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import Nav from "..";
+// __tests__/Nav.test.js with hard coded categories
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
 
 afterEach(cleanup);
 
 describe('Nav component', () => {
-    // baseline test
-    test('renders', () => {
-        render(<Nav />);
-    });
-    //snapshot test
-    test('matches snapshot', () => {
-        const { asFragment } = render(<Nav />);
-        expect(asFragment()).toMatchSnapshot();
-    })
+  it('renders', () => {
+    render(<Nav />);
+  });
+
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Nav />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
 })
 
 describe('emoji is visible', () => {
-    test('inserts emoji into the h2', () => {
+  it('inserts emoji into the h2', () => {
+    const { getByLabelText } = render(<Nav />);
 
-        const { getByLabelText } = render(<Nav />);
-
-        expect(getByLabelText('camera')).toHaveTextContent('📸');
-    })
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
+  });
 })
 
 describe('links are visible', () => {
-    test('inserts text into the links', () => {
-    
+  it('inserts text into the links', () => {
     const { getByTestId } = render(<Nav />);
-        expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
-        expect(getByTestId('about')).toHaveTextContent('About me');
-    });
+
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
+  });
+
 })
