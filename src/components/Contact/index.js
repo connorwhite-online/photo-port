f06fdb90c320aceaa07b2b9c0be3080d9924f60a
Connor White <connorwhitepdx@gmail.com>
2022-07-25 21:07:46 -0700

    client side form validation

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import { validateEmail } from "../../utils/helpers";
+
 function ContactForm () {
 
     const [formState, setFormState] = useState({
@@ -8,15 +10,37 @@ function ContactForm () {
         message: ""
     });
 
+    const [errorMessage, setErrorMessage] = useState('');
+
     const { name, email, message } = formState;
 
     function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value})
+
+        if (e.target.name=== 'email') {
+
+            const isValid = validateEmail(e.target.value);
+           
+            if (!isValid) {
+                setErrorMessage('Please enter a valid email address');
+            } else {
+                setErrorMessage('');
+            } 
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+
+        if (!errorMessage) {
+            setFormState({...formState, [e.target.name]: e.target.value});
+        }
+        
     }
 
     function handleSubmit(e) {
         e.preventDefault();
-        console.log(formState);
     }
     
     return (
